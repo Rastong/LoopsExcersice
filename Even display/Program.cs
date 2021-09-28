cdb97c60fc8fe516b6b2165a862c966cfef6436d
Raston Gilbert <rastong@gmail.com>
2021-09-28 13:48:01 -0400

    first commit

---
+++ Even display/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Even_display
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i <= 100; i++)
+            {
+                int OddOrEven = i % 2;
+                if(OddOrEven == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
